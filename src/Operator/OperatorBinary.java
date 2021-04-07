3699367ab873a50691d3f5e4563c52d8cd5fdc16
RINA <81545047+rinaapriani@users.noreply.github.com>
2021-04-07 12:29:05 +0800

    Operator Binary Assignment

---
+++ src/Operator/OperatorBinary.java
@@ -0,0 +1,12 @@
+package Operator;
+
+public class OperatorBinary {
+
+    //operator (=) sebagai assigment/penugasan
+    //menampilkan nilai kedalam variabel
+    //baris 5-7 adalah comment/komentar yang tidak dijalankan
+
+    String nama = "Rina Apriani";
+    int jumlahSks = 24;
+    double indeksPrestasiKomulatif = 3.89;
+}
